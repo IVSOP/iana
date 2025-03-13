ea948ba243ddbf7ddfea640cdef7c03e5f83f99f
Ivan Ribeiro <ivan.ribeiro09s@gmail.com>
2025-03-13 17:42:51 +0000

    init

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var exports2json = require('gulp-exports2json'),
-    gulp = require('gulp');
-
-gulp.task('default', function () {
-    return gulp
-        .src(['src/*.js'])
-        .pipe(exports2json())
-        .pipe(gulp.dest('themes/'));
-});
