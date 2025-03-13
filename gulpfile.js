6cd624cc479a84f4486e812628939b603b11232f
Ivan Ribeiro <ivan.ribeiro09s@gmail.com>
2025-03-13 19:43:07 +0000

    js to json, changed colors

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var exports2json = require('gulp-exports2json'),
+gulp = require('gulp');
+
+gulp.task('default', function () {
+    return gulp
+        .src(['src/*.js'])
+        .pipe(exports2json())
+        .pipe(gulp.dest('themes/'));
+});
+
